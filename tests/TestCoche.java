16b0bf1b2c02c0bc5f0bde1ad02c3cac0ae1d150
Main <danieljuan2002@gmail.com>
2023-03-21 16:30:20 +0100

    Modificacion nombre de los metodos test

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,24 +3,24 @@ import org.junit.Test;
 
 public class TestCoche {
     @Test
-    public void test_crearCoche() {
+    public void test_crearCoche_DanielMiralles() {
         Coche nuevoCoche = new Coche();
     }
 
     @Test
-    public void test_AlCrearCoche_SuVelocidad_esCero() {
+    public void test_AlCrearCoche_SuVelocidad_esCero_DanielMiralles() {
         Coche nuevoCoche = new Coche();
         Assert.assertEquals(0, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_AlAcelerar_UnCoche_SuVelocidad_Aumenta() {
+    public void test_AlAcelerar_UnCoche_SuVelocidad_Aumenta_DanielMiralles() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.acelerar(30);
         Assert.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_AlDecelerar_UnCoche_SuVelocidad_Disminuye_NoPuedeSer_MenorQue0() {
+    public void test_AlDecelerar_UnCoche_SuVelocidad_Disminuye_NoPuedeSer_MenorQue0_DanielMiralles() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.deacelerar(80);
