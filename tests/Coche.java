6e99bd6bf936c7db835e3be6c34bc78b6d98271c
Main <danieljuan2002@gmail.com>
2023-03-21 15:59:26 +0100

    Creamos un nuevo metodo test llamado test_AlCrearCoche_SuVelocidad_esCero y creamos la variable velocidad en la clase Coche.

--- tests/Coche.java
+++ tests/Coche.java
@@ -1,3 +1,4 @@
 public class Coche {
 
+    public int velocidad;
 }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,8 +1,14 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCoche {
     @Test
     public void test_crearCoche(){
         Coche nuevoCoche = new Coche();
+    }
+    @Test
+    public void test_AlCrearCoche_SuVelocidad_esCero(){
+        Coche nuevoCoche = new Coche();
+        Assert.assertEquals(0,nuevoCoche.velocidad);
     }
 }
